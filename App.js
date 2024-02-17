3381d2fcbb5919d7f8b2d842265fda9ab3da4bfb
arunviswam88 <arunviswam88@gmail.com>
2024-02-17 09:57:56 +0530

    React learnings

---
+++ App.js
@@ -0,0 +1,3 @@
+header = React.createElement("h1",{id:"header", abc:"xyz", style:{color:"red"}},"Hello from React");
+dom = ReactDOM.createRoot(document.getElementById('root'));
+dom.render(header);
