a15d161d58cf89c77c52612bc0836836facdaace
arunviswam88 <arunviswam88@gmail.com>
2024-02-19 18:56:35 +0530

    Functional component

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import  React  from 'react';
 import  ReactDOM  from 'react-dom/client';
 
-header = React.createElement("h1",{id:"header", abc:"xyz", style:{color:"red"}},"Hello from React");
-dom = ReactDOM.createRoot(document.getElementById('root'));
-dom.render(header);
+heading = (<h1>Hello from React element</h1>);
+
+FunctionComponent = ()=> (
+    <><h1>This is my first function component</h1>
+    <span>{heading}</span></>
+    
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<FunctionComponent/>);
